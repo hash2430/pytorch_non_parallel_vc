0a5b5ff24902c3dcbda0978d6a2913dcab40e1cc
Sunghee Jung <hash2430@gmail.com>
2019-05-01 11:55:17 +0900

    Purpose: network modification, performance tuning 1. conv1d_instance_norm - nn.Conv1d 뒤에 nn.InstanceNorm을 호출하고 있었지만,  pytorch 1.0.1에서는 nn.BatchNorm1d를 호출할 수 있기 때문에 nn.BatchNorm1d를 호출했다. - 예전 버전에서도 지원됐던 functional.batch_norm은 running average와 running mean을 직접 구현해야 했기 때문에 불편함이 있었다. TODO: 함수 이름을 고쳐야겠다.
    2. conv1d_banks()에서 convolution bank들의 output을 통합하기 위해 Tensor.view() 할 때
    미심쩍은 부분 수정
    - 기존에는 (K, N, C, T)로부터 view operation을 통해
    (N, K*C, T)로 변환하도록 수정하고 있었다. 그런데 view operation이 마법처럼
    이 모든 문제를 해결할 수 있는지 잘 모르겠어서 view()전에 K와 N의 transpose를 먼저
    수행하도록 코드를 수정했다.
    - (K, N, C, T) =>(N, K, C, T) =>(N, K*C, T)
    - view()를 하기 전에 메모리의 연속성을 보장하기 위해 contiguous()를 해야한다.
    TEST: 그런데 이 코드가 실제로 필요했는지 테스트는 해보지 않았다.
    
    3. model에서 CBHG 전에 호출되는 prenet에 batch normaliation 추가
    - 2 layer로 된 fully connected network인데, 1개의 layer마다
    batch normalization이 일어나도록 코드를 추가했다.
    - Fully connected network는 (N,T,C)  디멘젼에 일어나기 때문에
    output을 (N, C, T) 디멘젼으로 transpose 후에 batch normalization을 해야 한다.
    - 순서는 Fully connected network - dropout - batch normalization - relu
    의 순으로 하였다.
    - Batch normalization과 relu를 어느것을 먼저해야하는지는 연구하는 사람마다 의견이 다르다.
    - 하지만 본래 제안된 논문에서는 batch normalization 후에 relu를 할 것을 제안하고 있다.
    
    4. CBHG
    - Convolutional bank에 통과 시키고, 시간 축에서 maxPooling을 한 뒤에 convolution network를 통과시킬 때
    기존에 CBHG.conv1d가 conv1d_instance_norm을 사용함으로써 relu activation은 forward에서 하고 있었던 것을 수정,
    activation을 포함하게 CBHG.conv1d를 정의했다. 기능상의 차이는 없음.
    - conv1d2 삭제: 기존 코드에서는 maxpool 결과물에 conv1d를 연달아 두번 했다.
    원래 CBHG 논문에는 1번만 나와있어서 이를 삭제했다.
    - highwaynet, gru 등의 input size에 대해 in_channel 대신 hidden_units이라는 변수로 사용함.
    in_channel은 CBHG 전체 모듈의 input size라는 뜻으로만 사용하고 싶어서
    중간 unit에서 쓰는 input size는 hidden size라는 이름으로 참조했다.
     CBHG에서 highwaynet, gru의 input dimension과 CBHG의 input dimension이 지금은 갖게 사용하고 있지만,
    구조적으로 꼭 같을 필요는 없는 것이기 때문이다.

---
+++ modules2.py
@@ -0,0 +1,120 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class conv1d_instance_norm(nn.Module):
+    def __init__(self, in_channels, num_units, kernel_size, stride_size, padding):
+        super(conv1d_instance_norm, self).__init__()
+        out_channels = num_units
+        self.network = nn.Sequential(
+            nn.Conv1d(in_channels=in_channels, out_channels=out_channels,
+                      kernel_size=kernel_size,
+                      stride=stride_size,
+                      padding=padding),
+            nn.BatchNorm1d(out_channels)
+        )
+
+    def forward(self, input):
+        output = self.network(input)
+        return output
+
+class conv1d_banks(nn.Module):
+    def __init__(self, in_channels, num_units, K=8):
+        super(conv1d_banks, self).__init__()
+        self.moduleList = nn.ModuleList()
+        for i in range(1, K+1):
+            m = conv1d_instance_norm(in_channels, num_units, i, 1, i//2)
+            self.moduleList.append(m)
+    def forward(self, x):
+        outputs = []
+        # Call convolution banks
+        # Same input x to all the convolutions (parallel relationship, not sequential between convolution banks)
+        for i, m in enumerate(self.moduleList):
+            output = m(x)
+            output = F.relu(output)
+            if i == 0:
+                l = output.data.shape[-1]
+            output = output[:,:,:l]
+            outputs.append(output)
+
+        # TODO: concatenate at the last dimension to give (N, Hp.embed_size*K//32, T)
+        output_shape = output.size() # (N, C, T)
+        outputs = torch.stack(outputs) # (K, N, C, T)
+        num_banks = len(outputs)
+        outputs = outputs.transpose(1, 0) # (N, K, C, T)
+        outputs = outputs.contiguous().view(output_shape[0], output_shape[1] * num_banks, output_shape[2])
+        return outputs
+class prenet(nn.Module):
+    def __init__(self, in_dim, hidden_units1, hidden_units2, dropout_rate):
+        super(prenet, self).__init__()
+        self.dropout_rate = dropout_rate
+        self.fc1 = nn.Linear(in_dim, hidden_units1)
+        self.fc2 = nn.Linear(hidden_units1, hidden_units2)
+        self.batch_norm1 = nn.BatchNorm1d(hidden_units1)
+        self.batch_norm2 = nn.BatchNorm1d(hidden_units2)
+    def forward(self, x):
+        output = self.fc1(x)
+        output = F.dropout(output, p=self.dropout_rate).transpose(1, 2)
+        output = self.batch_norm1(output)
+        output = F.relu(output).transpose(1, 2)
+
+        output = self.fc2(output)
+        output = F.dropout(output, p=self.dropout_rate).transpose(1, 2)
+        output = self.batch_norm2(output)
+        output = F.relu(output).transpose(1, 2)
+
+        return output
+
+class highwaynet(nn.Module):
+    def __init__(self, in_features, num_units):
+        super(highwaynet, self).__init__()
+        self.H = nn.Sequential(
+            nn.Linear(in_features=in_features, out_features=num_units),
+            nn.ReLU()
+        )
+        self.T = nn.Sequential(
+            nn.Linear(in_features=in_features, out_features=num_units, bias=True),
+            nn.Sigmoid()
+        )
+    def forward(self, x):
+        h = self.H(x)
+        t = self.T(x)
+        t_ = 1. - t
+        output = h * t + x * t_
+        return output
+
+class CBHG(nn.Module):
+    def __init__(self, in_channels, num_banks, hidden_units,
+               num_highway_blocks, kernel_size, stride_size, padding_size):
+        super(CBHG, self).__init__()
+        self.num_highway_blocks = num_highway_blocks
+        self.conv1d_banks = conv1d_banks(in_channels=in_channels,
+                                         num_units=hidden_units, K=num_banks)
+        self.maxPool1d = nn.MaxPool1d(kernel_size=kernel_size, stride=stride_size, padding=padding_size)
+        self.conv1d = nn.Sequential(nn.Conv1d(in_channels * num_banks, hidden_units,
+                                           kernel_size=kernel_size, stride=stride_size,
+                                              padding=padding_size),
+                                    nn.BatchNorm1d(hidden_units),
+                                    nn.ReLU())
+        # conv1d2 removed
+        self.highwaynet = highwaynet(hidden_units, hidden_units)
+        self.gru = nn.GRU(input_size=hidden_units, hidden_size=hidden_units, bidirectional=True)
+
+    def forward(self, input):
+        output = self.conv1d_banks(input) #(N, K*E/2, T)
+        output = self.maxPool1d(output)
+        output = self.conv1d(output)
+        output += input # residual connections
+
+        # Reshape: Highway input must be in shape (N, T, C)
+        output = output.transpose(2, 1)
+        # Highway
+        for i in range(self.num_highway_blocks):
+            output = self.highwaynet(output) # (N, T, E/2)
+
+        # Reshpae: GRU intput comes in the shape of (T, N, C)
+        output = output.transpose(1, 0)
+
+        # GRU
+        output, _ = self.gru(output)
+        return output
